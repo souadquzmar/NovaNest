72637b4e1f4ae7734a819e714902892e16db5fb3
souad <souadquzmar13@gmail.com>
2025-06-28 01:43:47 +0300

    modal design and logic

--- assets/home-products.js
+++ assets/home-products.js
@@ -8,7 +8,7 @@ async function getProducts() {
         (ele) =>
           `
             <div class="product">
-            <img src="${ele.thumbnail}" />
+            <img src="${ele.thumbnail}" class="product-img"/>
             <h3>${ele.title}</h3>
             <span>$${ele.price}</span>
             <a href="product-details.html?id=${ele.id}">Show Details</a>
@@ -17,7 +17,59 @@ async function getProducts() {
       )
       .join("");
     document.querySelector(".categorie-products").innerHTML = data;
+    customModal();
   } catch (err) {}
 }
 
 getProducts();
+
+function customModal() {
+  const images = Array.from(document.querySelectorAll(".product-img"));
+  const modal = document.querySelector(".my-modal");
+  const closeBtn = document.querySelector(".close-btn");
+  const leftBtn = document.querySelector(".left-btn");
+  const rightBtn = document.querySelector(".right-btn");
+  let index = null;
+  const modalImg = document.querySelector(".modal-img");
+  images.forEach(
+    (img) =>
+      (img.onclick = function (e) {
+        modalImg.setAttribute("src", e.target.getAttribute("src"));
+        modal.classList.remove("d-none");
+        index = images.indexOf(e.target);
+      })
+  );
+
+  function previousImg() {
+    if (index == 0) index = images.length - 1;
+    else index--;
+    modalImg.setAttribute("src", images[index].getAttribute("src"));
+  }
+
+  function nextImg() {
+    if (index == images.length - 1) index = 0;
+    else index++;
+    modalImg.setAttribute("src", images[index].getAttribute("src"));
+  }
+  closeBtn.onclick = function () {
+    modal.classList.add("d-none");
+  };
+
+  leftBtn.onclick = function () {
+    previousImg();
+  };
+
+  rightBtn.onclick = function () {
+    nextImg();
+  };
+
+  document.onkeydown = (e) => {
+    if (e.key == "ArrowRight") {
+      nextImg();
+    } else if (e.key == "ArrowLeft") {
+      previousImg();
+    } else if (e.key == "Escape") {
+      modal.classList.add("d-none");
+    }
+  };
+}
